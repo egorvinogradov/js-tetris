129959e677680ec802e52619c31435674d4d689f
Egor Vinogradov <1618344+egorvinogradov@users.noreply.github.com>
2022-01-09 23:23:42 +0700

    Code from yesterday

---
+++ tetris.js
@@ -0,0 +1,120 @@
+let MATRIX_WIDTH = 10;
+let MATRIX_HEIGTH = 15;
+let MATRIX = [];
+let FIGURES = {
+  I_1: [
+    [1],
+    [1],
+    [1],
+    [1],
+  ],
+  T_1: [
+    [0,1,0],
+    [1,1,1],
+  ],
+  Z_1: [
+    [1,1,0],
+    [0,1,1],
+  ],
+};
+let CURRENT_FIGURE = [];
+let CURRENT_FIGURE_TYPE = null;
+let CURRENT_FIGURE_X = 0;
+let CURRENT_FIGURE_Y = 0;
+let CURRENT_FIGURE_TIMEOUT = 2 * 1000;
+
+function spawnRandomFigure(){
+  const figureTypes = Object.keys(FIGURES);
+  const CURRENT_FIGURE_TYPE = figureTypes[random(figureTypes.length - 1)];
+  const CURRENT_FIGURE = FIGURES[CURRENT_FIGURE_TYPE];
+
+  const xMaxPos = MATRIX_WIDTH - CURRENT_FIGURE[0].length;
+
+  CURRENT_FIGURE_X = random(xMaxPos);
+  CURRENT_FIGURE_Y = 0;
+}
+
+function drawFigureOnMartix(figure){
+  console.log('drawFigureOnMartix', figure);
+};
+
+function random(limit){
+  return Math.round(Math.random() * limit);
+}
+
+function createArray(size, filler){
+  return [...new Array(size)].map(() => filler);
+}
+
+function clearMartix(){
+  const matrixRow = createArray(MATRIX_WIDTH, 0);
+  MATRIX = createArray(MATRIX_HEIGTH, matrixRow);
+}
+
+function moveFigureDown(){
+
+}
+
+
+function calculateDistanceBelow(){}
+
+function addFigureToMatrix(){}
+
+function rotateFigureClockwise(){}
+
+function renderMatrix(){
+  const tableHTML = MATRIX.map(row => {
+    console.log('row', row);
+    const rowHTML = row.map(cell => {
+      return `<td class="${ cell ? 'filled' : 'empty' }"></td>`;
+    }).join('\n');
+    return '  <tr>\n    ' + rowHTML + '\n</tr>';
+  }).join('\n');
+
+  const fullCode = `
+<style>
+table { border-collapse: collapse; }
+td { width: 10px; height: 10px; }
+.filled { background: black; }
+</style>
+<table>
+  ${tableHTML}
+</table>`;
+  window.fullCode = fullCode;
+  console.log({ fullCode });
+  document.body.innerHTML = fullCode;
+}
+
+function moveCurrentFigureByTimer(figure, callback){
+  window.figureTimeout = setTimeout(() => {
+    let distanceBelow = calculateDistanceBelow();
+    if (distanceBelow) {
+      CURRENT_FIGURE_Y++;
+      renderMatrix();
+      moveCurrentFigureByTimer(figure, callback);
+    }
+    else {
+      clearTimeout(window.figureTimeout);
+      callback();
+    }
+    
+
+  }, CURRENT_FIGURE_TIMEOUT);
+}
+
+
+clearMartix();
+renderMatrix();
+spawnRandomFigure();
+
+
+
+moveCurrentFigureByTimer(() => {
+  console.log('--- DONE ---');
+});
+
+
+
+
+
+
